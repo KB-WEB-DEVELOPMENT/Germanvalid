6f1edef1e77efaa93f3abf9345ff0be7e00c55df
Kami Barut-Wanayo <kamibarut@yahoo.com>
2020-07-01 08:25:44 +0200

    Update Germanvalid.php

--- src/Germanvalid.php
+++ src/Germanvalid.php
@@ -23,7 +23,45 @@
 		
 		const MOBILE_EXCEPTION_1 = "0176";
 		const MOBILE_EXCEPTION_2 = "01609";
+		
+		const GEOGRAPHIC_ZONE_NUMBER = "00";
+		
+		const FIRST_FOUR_DIGITS = "0000";
+		
+		const FIXED_NUMBERS = array("110","112","115","19222");
+		
+		const REGEX1 = '/^116[0-9]{3}$/';
+		
+		const REGEX2 = '/^118[0-9]{2}$/';
+		
+		const FIXED_FOUR_DIGITS_NUMBERS = array("0191","0192","0193","0194");
+		const FIXED_FIVE_CHAR_NUMBERS = "010xy";
+		const FIXED_SIX_CHAR_NUMBERS = "0100yy";
+		
+		const FIRST_THREE_DIGITS = "000";
+		
+		protected $input = "";
+		protected $length = 0;
+		
+		protected $geographic_zone_number = "";
+
+		protected $firstThreeDigits = "";
+		protected $firstFourDigits = "";
+		
+		protected $firstFourDigitsEntered = "";
+		protected $firstFiveDigitsEntered = "";
+		
+		protected $fixed_4_digits_numbers = "";
+		protected $fixed_5_char_numbers = "";
+		protected $fixed_6_char_numbers = "";
+		
+		protected $regex = "";
+		
+		protected $result = false;
 			
+		protected $message = "";
+			
+				
 			 /**
 			 * returns whether the entered input is a valid assignable German geographic phone number 
 			 *[Government run and controlled]
@@ -34,115 +72,115 @@
 			public function validate_geo_number($input) {
 		
 				// removes all non digits
-				$input = preg_replace('~\D~','', $input);
+				$this->input = preg_replace('~\D~','', $input);
 			
-				$length = strlen($input);
+				$this->length = (int)strlen($this->input);
 				
 				// say length < 11 or length > 12, incorrect mobile number - needed for the switch statement 
-				$geographic_zone_number = "00";
+				$this->geographic_zone_number = GEOGRAPHIC_ZONE_NUMBER;
 
 				// user included a geographic zone number, 11 digits total
-				if ($length == 11)  {
+				if ($this->length == 11)  {
 					
-					$geographic_zone_number = substr($input,0,2);
+					$this->geographic_zone_number = substr($this->input,0,2);
 				}
 				
 				// user included a geographic zone number, 12 digits total
-				if ($length == 12)  {
+				if ($this->length == 12)  {
 		
-					$geographic_zone_number = substr($input,0,2);
+					$this->geographic_zone_number = substr($this->input,0,2);
 				}
 								
-				switch($geographic_zone_number) {												
+				switch($this->geographic_zone_number) {												
 				
 					case '02':
-						$regex = '/^02[0-9]{9,10}$/';
+						$this->regex = '/^02[0-9]{9,10}$/';
 						break;
 
 					case '03':
-						$regex = '/^03[0-9]{9,10}$/';
+						$this->regex = '/^03[0-9]{9,10}$/';
 						break;	
 
 					case '04':
-						$regex = '/^04[0-9]{9,10}$/';
+						$this->regex = '/^04[0-9]{9,10}$/';
 						break;
 						
 					case '05':
-						$regex = '/^05[0-9]{9,10}$/';
+						$this->regex = '/^05[0-9]{9,10}$/';
 						break;
 						
 					case '06':
-						$regex = '/^06[0-9]{9,10}$/';
+						$this->regex = '/^06[0-9]{9,10}$/';
 						break;
 						
 					case '07':
-						$regex = '/^07[0-9]{9,10}$/';
+						$this->regex = '/^07[0-9]{9,10}$/';
 						break;				
 
 					case '08':
-						$regex = '/^08[0-9]{9,10}$/';
+						$this->regex = '/^08[0-9]{9,10}$/';
 						break;
 						
 					case '09':
-						$regex = '/^09[0-9]{9,10}$/';
+						$this->regex = '/^09[0-9]{9,10}$/';
 						break;					
 					
 					default:
-						$regex = '/^alwayswrong!!!!$/';
+						$this->regex = '/^alwayswrong!!!!$/';
 						break;						
 				}
 				
-				if (preg_match($regex, $input) == 1) {
-					$result = true;    
+				if (preg_match($this->regex, $this->input) == 1) {
+					$this->result = true;    
 				} else {    
-					$result = false;    
+					$this->result = false;    
 				  }	
 				  
 				// exclude all numbers with more than 12 digits
-				if ($length > 12) {
+				if ($this->length > 12) {
 					
-					$result = false;
+					$this->result = false;
 				}
 				
 				// exclude all numbers with less than 6 digits
-				if ($length < 6) {
+				if ($this->length < 6) {
 					
-					$result = false;
+					$this->result = false;
 				}
 				
 				// any 6 digits is correct, i.e: user entered the number without the geographic zone number
-				if ($length == 6) {
+				if ($this->length == 6) {
 					
-					$result = true;
+					$this->result = true;
 				
 				} 
 				// any 7 digits is correct, i.e: user entered the number without the geographic zone number
-				if ($length == 7) {
+				if ($this->length == 7) {
 					
-					$result = true;
+					$this->result = true;
 				
 				} 
 							  
 				// any 8 digits is correct, i.e: user entered the number without the geographic zone number
-				if ($length == 8) {
+				if ($this->length == 8) {
 					
-					$result = true;
+					$this->result = true;
 							
 				} 
 				// the user entered 9 digits, too few with the geographic zone number, too many without.
-				if ($length == 9) {
+				if ($thi->length == 9) {
 					
-					$result = false;
+					$this->result = false;
 				
 				} 
 				// the user entered 10 digits, too few with the geographic zone number, too many without.
-				if ($length == 10) {
+				if ($this->length == 10) {
 					
-					$result = false;
+					$this->result = false;
 					
 				}
 							
-				return $result;
+				return $this->result;
 			
 			}
 		
@@ -156,123 +194,123 @@
 			public function validate_mobile_operator($input) {
 
 				// removes all non digits
-				$input = preg_replace('~\D~','', $input);
+				$this->input = preg_replace('~\D~','', $input);
 			
-				$length = strlen($input);
+				$this->length = (int)strlen($this->input);
 
-				if ($length < 4)  {
+				if ($this->length < 4)  {
 		
-					$firstFourDigits = "0000"; // wrong first four digits, used in switch statement below 
+					$this->firstFourDigits = FIRST_FOUR_DIGITS; // wrong first four digits, used in switch statement below 
 				} 
 				
-				if ($length == 4)  {
+				if ($this->length == 4)  {
 					
-					$firstFourDigits = substr($input,0,4);
+					$this->firstFourDigits = substr($this->input,0,4);
 				
 				}
 				
-				if ($length > 4)  {
+				if ($this->length > 4)  {
 					
-					$firstFourDigits = substr($input,0,4);
+					$this->firstFourDigits = substr($this->input,0,4);
 				
 				}
 				
-				switch($firstFourDigits) {				
+				switch($this->firstFourDigits) {				
 					case '0150':
-						$regex = '/^0150[0-9]{7,8}$/';
+						$this->regex = '/^0150[0-9]{7,8}$/';
 						break;
 					case '0151':
-						$regex = '/^0151[0-9]{7,8}$/';
+						$this->regex = '/^0151[0-9]{7,8}$/';
 						break;				
 					case '0152':
-						$regex = '/^0152[0-9]{7,8}$/';
+						$this->regex = '/^0152[0-9]{7,8}$/';
 						break;			
 					case '0155':
-						$regex = '/^0155[0-9]{7,8}$/';
+						$this->regex = '/^0155[0-9]{7,8}$/';
 						break;			
 					case '0157':
-						$regex = '/^0157[0-9]{7,8}$/';
+						$this->regex = '/^0157[0-9]{7,8}$/';
 						break;		
 					case '0159':
-						$regex = '/^0159[0-9]{7,8}$/';
+						$this->regex = '/^0159[0-9]{7,8}$/';
 						break;
 					case '0160':
-						$regex = '/^0160[0-9]{7,8}$/';
+						$this->regex = '/^0160[0-9]{7,8}$/';
 						break;					
 					case '0161':
-						$regex = '/^0161[0-9]{7,8}$/';
+						$this->regex = '/^0161[0-9]{7,8}$/';
 						break;		
 					case '0162':
-						$regex = '/^0162[0-9]{7,8}$/';
+						$this->regex = '/^0162[0-9]{7,8}$/';
 						break;		
 					case '0163':
-						$regex = '/^0163[0-9]{7,8}$/';
+						$this->regex = '/^0163[0-9]{7,8}$/';
 						break;
 					case '0164':
-						$regex = '/^0164[0-9]{7,8}$/';
+						$this->regex = '/^0164[0-9]{7,8}$/';
 						break;
 					case '0168':
-						$regex = '/^0168[0-9]{7,8}$/';
+						$this->regex = '/^0168[0-9]{7,8}$/';
 						break;
 					case '0169':
-						$regex = '/^0169[0-9]{7,8}$/';
+						$this->regex = '/^0169[0-9]{7,8}$/';
 						break;
 					case '0170':
-						$regex = '/^0170[0-9]{7,8}$/';
+						$this->regex = '/^0170[0-9]{7,8}$/';
 						break;
 					case '0171':
-						$regex = '/^0171[0-9]{7,8}$/';
+						$this->regex = '/^0171[0-9]{7,8}$/';
 						break;
 					case '0172':
-						$regex = '/^0172[0-9]{7,8}$/';
+						$this->regex = '/^0172[0-9]{7,8}$/';
 						break;					
 					case '0173':
-						$regex = '/^0173[0-9]{7,8}$/';
+						$this->regex = '/^0173[0-9]{7,8}$/';
 						break;					
 					case '0174':
-						$regex = '/^0174[0-9]{7,8}$/';
+						$this->regex = '/^0174[0-9]{7,8}$/';
 						break;					
 					case '0175':
-						$regex = '/^0175[0-9]{7,8}$/';
+						$this->regex = '/^0175[0-9]{7,8}$/';
 						break;					
 					case '0177':
-						$regex = '/^0177[0-9]{7,8}$/';
+						$this->regex = '/^0177[0-9]{7,8}$/';
 						break;					
 					case '0178':
-						$regex = '/^0178[0-9]{7,8}$/';
+						$this->regex = '/^0178[0-9]{7,8}$/';
 						break;					
 					case '0179':
-						$regex = '/^0179[0-9]{7,8}$/';
+						$this->regex = '/^0179[0-9]{7,8}$/';
 						break;
 					default:
-						$regex = '/^alwayswrong!!!!$/';
+						$this->regex = '/^alwayswrong!!!!$/';
 						break;						
 				}
 				
-				if (preg_match($regex, $input) == 1) {
-					$result = true;    
+				if (preg_match($this->regex, $this->input) == 1) {
+					$this->result = true;    
 				} else {    
-					$result = false;    
+					$this->result = false;    
 				  }
 				
 				// checking for exceptions - mandatory 12 digits for 0176 - xxx - xxxxx and for 01609 - xxx - xxxx
-				if ($length == 11) {
+				if ($this->length == 11) {
 					
-					$firstFourDigitsEntered = substr($input,0,4);
-					$firstFiveDigitsEntered = substr($input,0,5);
+					$this->firstFourDigitsEntered = substr($this->input,0,4);
+					$this->firstFiveDigitsEntered = substr($this->input,0,5);
 				}	
 			
-				if ($firstFourDigitsEntered == MOBILE_EXCEPTION_1) {
+				if ($this->firstFourDigitsEntered == MOBILE_EXCEPTION_1) {
 
-					$result = false;
+					$this->result = false;
 				}
 				
-				if ($firstFiveDigitsEntered == MOBILE_EXCEPTION_2) {
+				if ($this->firstFiveDigitsEntered == MOBILE_EXCEPTION_2) {
 
-					$result = false;
+					$this->result = false;
 				}
 				
-				return $result;
+				return $this->result;
 			}
 
 			 /**
@@ -282,31 +320,27 @@
 			 */
 		
 			public function validate_emergency($input)	{
-							
-				$fixed_numbers =array("110","112","115","19222");
-				$regex1 = '/^116[0-9]{3}$/';
-				$regex2 = '/^118[0-9]{2}$/';
+											
+				$this->input = preg_replace('~\D~','',$input);
 				
-				$input = preg_replace('~\D~','',$input);
+				if (in_array($this->input, FIXED_NUMBERS)) {
+					
+					$this->result = true;
+					
+				} elseif (preg_match(REGEX1, $this->input) == 1) {
+					
+					$this->result = true;
 				
-				if (in_array($input, $fixed_numbers)) {
+				} elseif (preg_match(REGEX2, $this->input) == 1) {
 					
-					$result = true;
-					
-				} elseif (preg_match($regex1, $input) == 1) {
-					
-					$result = true;
-				
-				} elseif (preg_match($regex2, $input) == 1) {
-					
-					$result = true;
+					$this->result = true;
 				
 				} else {
 					
-					$result = false;
+					$this->result = false;
 				}
 				
-				return $result;
+				return $this->result;
 				
 			}
 			
@@ -317,89 +351,84 @@
 			 */
 			
 			public function validate_non_geo($input) {
-
-				// fixed set length correct numbers 
-				$fixed_4_digits_numbers =array("0191","0192","0193","0194");
-				$fixed_5_char_numbers ="010xy";
-				$fixed_6_char_numbers ="0100yy";
 			
 				// removes all non digits
-				$input = preg_replace('~\D~','', $input);
+				$this->input = preg_replace('~\D~','', $input);
 			
-				$length = strlen($input);
+				$this->length = (int)strlen($this->input);
 									
-				if ($length < 3)  {
+				if ($this->length < 3)  {
 		
-					$firstThreeDigits = "000"; // incorrect first three digits, used in switch statement
+					$this->firstThreeDigits = FIRST_THREE_DIGITS; // incorrect first three digits, used in switch statement
 				} 
 					
-				$firstThreeDigits = substr($input,0,3);
+				$this->firstThreeDigits = substr($this->input,0,3);
 				
-				switch($firstThreeDigits) {												
+				switch($this->firstThreeDigits) {												
 					case '011':
-						$regex = '/^011[0-9]{1,9}$/';
+						$this->regex = '/^011[0-9]{1,9}$/';
 						break;
 					case '012':
-						$regex = '/^012[0-9]{9,}$/';
+						$this->regex = '/^012[0-9]{9,}$/';
 						break;
 					case '013':
-						$regex = '/^013(?=7)[0-9]{10}|(?=8)1[0-9]{1,}$/';
+						$this->regex = '/^013(?=7)[0-9]{10}|(?=8)1[0-9]{1,}$/';
 						break;
 					case '018':
-						$regex = '/^018(?=0)[0-9]{7}|[1-9]{1,}$/';
+						$this->regex = '/^018(?=0)[0-9]{7}|[1-9]{1,}$/';
 						break;
 					case '019':
-						$regex = '/^019(?=8)[0-9]{1,}|(?=9)[0-9]{1,}|[0-9]{3}$/';
+						$this->regex = '/^019(?=8)[0-9]{1,}|(?=9)[0-9]{1,}|[0-9]{3}$/';
 						break;
 					case '031':
-						$regex = '/^031[0-9]{1}$/';
+						$this->regex = '/^031[0-9]{1}$/';
 						break
 					case '032':
-						$regex = '/^032[0-9]{9}$/';
+						$this->regex = '/^032[0-9]{9}$/';
 						break;
 					case '070':
-						$regex = '/^0700[0-9]{8}$/';
+						$this->regex '/^0700[0-9]{8}$/';
 						break;
 					case '080':
-						$regex = '/^0800[0-9]{7}$/';
+						$this->regex '/^0800[0-9]{7}$/';
 						break;
 					case '090':
-						$regex = '/^09000(?=9)[0-9]{7}|[0-9]{7}$/';
+						$this->regex '/^09000(?=9)[0-9]{7}|[0-9]{7}$/';
 						break;
 					default:
-						$regex = '/^alwayswrong!!!!$/';
+						$this->regex '/^alwayswrong!!!!$/';
 						break;						
 				}
 				
-				if (preg_match($regex, $input) == 1) {
+				if (preg_match($this->regex, $this->input) == 1) {
 					
-					$result = true;    
+					$this->result = true;    
 				
 				} else {    
 				
-				$result = false;    
+				$this->result = false;    
 
 				}
 							
-				if (in_array($input, $fixed_4_digits_numbers)) {
+				if (in_array($this->input, FIXED_FOUR_DIGITS_NUMBERS)) {
 					
-					$result = true;
+					$this->result = true;
 				
 				} 
 				
-				if ($input == $fixed_5_char_numbers) {
+				if ($this->input == FIXED_FIVE_CHAR_NUMBERS) {
 					
-					$result = true;
+					$this->result = true;
 				
 				} 
 				
-				if ($input == $fixed_6_char_numbers) {
+				if ($input == FIXED_SIX_CHAR_NUMBERS) {
 					
-					$result = true;
+					$this->result = true;
 				
 				} 
 				
-				return $result;
+				return $this->result;
 				  
 			}
 		
@@ -412,30 +441,31 @@
 	public function printPhoneNumberType($input)	{
 
 		// removes all non digits
-		$input = preg_replace('~\D~','', $input);
+		$this->input = preg_replace('~\D~','', $input);
 
-		if ($this->validate_geo_number($input) == true) {
+		if ($this->validate_geo_number($this->input) == true) {
 	
-			$message = "Valid German geographically based phone number format";
+			$this->message = "Valid German geographically based phone number format";
 	
-		} elseif ($this->validate_mobile_operator($input) == true) {
+		} elseif ($this->validate_mobile_operator($this->input) == true) {
 				
-			$message = "Valid German commercial mobile phone number format";
+			$this->message = "Valid German commercial mobile phone number format";
 			
-		  } elseif ($this->validate_emergency($input) == true) {
+		  } elseif ($this->validate_emergency($this->input) == true) {
 				
-			$message = "Valid German emergency/government service phone number format";
+			$this->message = "Valid German emergency/government service phone number format";
 			
-		   } elseif ($this->validate_non_geo($input) == true) {
+		   } elseif ($this->validate_non_geo($this->input) == true) {
 					
-			$message = "Valid German non-geographically based phone number format";
+			$this->message = "Valid German non-geographically based phone number format";
 	
 		   } else {
 		
-			$message = "Unknown German phone number format";
-		     }
+			$this->message = "Unknown German phone number format";
+		     
+			}
 		
-		echo $message;
+		echo $this->message;
 		
 	 }
 		
